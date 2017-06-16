754fced8d4b851c5c64ff89bd76050c8ed8b25f0
shorti1996 <shorti1996@gmail.com>
2017-06-16 12:49:00 +0200

    Initial commit

---
+++ app/src/main/java/com/shorti1996/testownik/InTestActivity.java
@@ -0,0 +1,13 @@
+package com.shorti1996.testownik;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InTestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_test);
+    }
+}
